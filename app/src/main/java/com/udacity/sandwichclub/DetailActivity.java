6fdf4778b5edaf6e5e4a04cd21af919fe5b24152
live-beta <wanjala.sam.s@gmail.com>
2018-07-30 22:32:17 +0300

    (add): adds and modifies the json
    - Adds a data layer for fetching json and persing it
    - Creates a seperate thread to handle data calls
    
    [Builds #Task-data-layer]

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,15 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONException;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -19,7 +22,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        Log.d("Executing this", "Executing this");
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
@@ -29,21 +32,24 @@ public class DetailActivity extends AppCompatActivity {
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
             closeOnError();
             return;
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = null;
+        Log.d("getting sandwich","getting sandwich");
+       // sandwich = JsonUtils.parseSandwichJson(json);
+        Log.d("data parseding", sandwich.getDescription());
         if (sandwich == null) {
             // Sandwich data unavailable
+
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +62,7 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,42 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    private static Sandwich sandwich = new Sandwich();
+    private static JSONArray sandwichData;
+
+    public static void jsonDataStream(String[] json) throws JSONException {
+        JSONArray sandwichArray = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            sandwichData = new JSONArray(json);
+        }
+
     }
+
+    public static Sandwich parseSandwichJson(ArrayList dataCurrent) throws JSONException {
+
+           sandwich.setDescription();
+           sandwich.setPlaceOfOrigin();
+           sandwich.setImage();
+           sandwich.setIngredients();
+           sandwich.setAlsoKnownAs();
+           sandwich.setMainName();
+
+
+        return sandwich;
+    }
+
+
 }
